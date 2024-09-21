0dd09fb1d3e3cfe8efe3519744d21f0fd525b68d
Anik Datta <auniikq@gmail.com>
2024-09-21 16:36:38 +0600

    Updated docker file

--- bootstrap/core/Request.php
+++ bootstrap/core/Request.php
@@ -23,13 +23,14 @@ class Request
         static::$server = $server;
 
         $instance = new static();
+        $qs = $server['QUERY_STRING'] ?? '';
         $instance->method = $server['REQUEST_METHOD'];
-        $instance->scheme = $server['REQUEST_SCHEME'];
+        $instance->scheme = $server['REQUEST_SCHEME'] ?? 'http';
         $instance->uri = $server['REQUEST_URI'];
-        parse_str($server['QUERY_STRING'], $instance->query);
+        parse_str($qs, $instance->query);
         $instance->raw = $_POST ?: file_get_contents('php://input');
         $instance->body = is_string($instance->getRaw()) ? json_decode($instance->getRaw()) : $instance->getRaw();
-        $instance->queryString = $server['QUERY_STRING'] ?: null;
+        $instance->queryString = $qs ?: null;
         $instance->headers = getallheaders();
 
         return $instance;
